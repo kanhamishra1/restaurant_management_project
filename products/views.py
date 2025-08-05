239df9fbbae7cdc865408c628c61ee033f540b45
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-06 01:01:10 +0530

    20636

--- products/views.py
+++ products/views.py
@@ -1,27 +1,44 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
+# from django.shortcuts import render
+# from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+# from rest_framework import status
 from rest_framework.decoraters import api_view
 
-from .models import Item
-from .serializers import ItemSerializer
+# from .models import Item
+# from .serializers import ItemSerializer
 
-'''
-NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
-'''
+# '''
+# NOTE: Conside this as a reference and follow this same coding structure or format to work on you tasks
+# '''
 
-# Create your views here.
-class ItemView(APIView):
+# # Create your views here.
+# class ItemView(APIView):
 
-    def get(self, request):
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#     def get(self, request):
+#         items = Item.objects.all()
+#         serializer = ItemSerializer(items, many=True)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request):
-        serializer = ItemSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request):
+#         serializer = ItemSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def hardcoded_menu(request):
+    menu = [
+        {
+            "name" : "margherita pizza"
+            "description" : "Classic pizza with cheese and tomato sauce"
+            "price" : 250
+        },
+
+        {
+            "name" : "paneer butter masala"
+            "description" : "Creamy paneer in buttery tomato gravy"
+            "price" : 300
+        },
+    ]
+    return Response(menu)
