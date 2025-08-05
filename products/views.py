7e5abe3b545dd70965ae9081eb3cc15e5e3518ae
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-06 00:43:27 +0530

    20636

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decoraters import api_view
 
 from .models import Item
 from .serializers import ItemSerializer
