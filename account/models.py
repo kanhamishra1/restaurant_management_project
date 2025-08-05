972d21b51d12ccd6df2ae13ebd31397274c51874
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-06 00:18:38 +0530

    20632

--- account/models.py
+++ account/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) #User already have a email field
     name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_field=15, blank=True)
+    phone_number = models.CharField(max_length=15, null=True)
 
     def __str__(self):
-        return self.user.username
+        return self.user.get_username()
 
 
         
