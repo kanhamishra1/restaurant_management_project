506ae56d3ffeb4cd67ad08c66ff3462c779323fb
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-06 19:44:09 +0530

    20634

--- orders/models.py
+++ orders/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Menu
+from django.contrib.auth.models import User
 
 class Order(models.Model):
     menu_item = models.ForeignKey(Menu, on_delete=models.CASCADE)
@@ -8,5 +9,23 @@ class Order(models.Model):
 
     def __str__(self):
         return f"{self.menu_item.name} * {self.quantity}"
+
+class Order(models.Model):
+    STATUS_CHOICES = [
+
+        ('PENDING','pending'),
+        ('CONFIRMED','confirmed'),
+        ('CANCELLED','cancelled'),
+        ('DELIVERED','delevered'),
+    ]
+
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_items = models.ManyToManyField(Menu)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    order_status = models.CharField(max_length=20, choices=STATUS_CHOICES,default='PENDING')
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.pk} by {self.customer.username}" 
 
 
