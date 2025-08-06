8048363cb27caf61a430e6e821dea41cb5d93ab3
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-06 18:55:39 +0530

    20635

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from products.models import Menu
 
-# Create your models here.
+class Order(models.Model):
+    menu_item = models.ForeignKey(Menu, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    ordered_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.menu_item.name} * {self.quantity}"
+
+
