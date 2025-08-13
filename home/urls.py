a65fcdfc80f301e302896c00eb8bbf42592ffe72
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-13 14:23:40 +0530

    30684

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', home_view, name = 'home'),
-    path('home1/', )
+    path('home1/', demo_home_page,  )
     path('about/', about_view, name = 'about')
 ]
