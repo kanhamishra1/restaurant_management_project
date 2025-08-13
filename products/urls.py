83d879414fd15df104fe4a63c38915470293827e
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-13 15:32:38 +0530

    30680

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,6 @@ from .views import *
 urlpatterns = [
     path('', home, name='home'),
     path('items/', ItemView.as_view(), name='item-list'),
-    path('menu/' ,  , name='hardcoded_menu'),
+    path('menu/' , menu_item , name='hardcoded_menu'),
     
 ]
