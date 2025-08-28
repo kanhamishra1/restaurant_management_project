930d095212071e3457405f611270149737a7f681
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-28 23:12:56 +0530

    39121

--- home/context_processors.py
+++ home/context_processors.py
@@ -1,4 +1,8 @@
 import datetime
+from django.conf import settings
 
 def current_year(request):
     return {'current_year': datetime.datetime.now().year}
+
+def restaurant_name(request):
+    return {"RESTAURANT_NAME" : getattr(settings, "RESTAURANT_NAME", "restaurant")}
