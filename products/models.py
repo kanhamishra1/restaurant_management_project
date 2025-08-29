63605e1df4707021247a2747265ab3ca45ab4820
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-29 23:20:11 +0530

    39119

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Item(models.Model):
     item_name = models.CharField(max_length=150)
     item_price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -8,6 +8,8 @@ class Item(models.Model):
 
     def __str__(self):
         return str(self.item_name)
+        
+# This is menu item model
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=150)
