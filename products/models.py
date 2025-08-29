3a43d251b1dabe5f2f7c545140b57a573a91e57c
kanhamishra1 <kanhamishra1a@gmail.com>
2025-08-29 23:20:47 +0530

    39119

--- products/models.py
+++ products/models.py
@@ -14,7 +14,7 @@ class Item(models.Model):
 class MenuItem(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
-    price = models.DecimalField(max_digits=10 ,decimal_places=2)
+    price = models.DecimalField(max_digits=15 ,decimal_places=2)
 
     def __str__(self):
         return self.name
