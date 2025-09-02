a55347c653858548f3136d26b22d85bcff9fe6c2
kanhamishra1 <kanhamishra1a@gmail.com>
2025-09-02 20:13:04 +0530

    40609

--- products/models.py
+++ products/models.py
@@ -15,6 +15,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=15 ,decimal_places=2)
+    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
 
     def __str__(self):
         return self.name
