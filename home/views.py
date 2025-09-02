fd92cea55a61c5eb0c30e6f9e744fa4037fea0af
kanhamishra1 <kanhamishra1a@gmail.com>
2025-09-02 20:47:39 +0530

    40607

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponseServerError
 from .forms import ContactForm
+from .models import 
 
 
 def home_view(request):
