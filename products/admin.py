4e272c250c59f2b479bd7da657dd75d243af6e2b
kanhamishra1 <kanhamishra1a@gmail.com>
2025-09-02 20:37:32 +0530

    40608

--- products/admin.py
+++ products/admin.py
@@ -6,7 +6,7 @@ from .models import Item
 admin.site.register(MenuItem)
 
 @admin.register(MenuItem)
-class MenuItemAdmin(admin.ModelAdmin)
+class MenuItemAdmin(admin.ModelAdmin):
     list_display = ('name', 'price')
 
 
