ff3b12015bd402ff5bb891334b73df5032877a14
kanhamishra1 <kanhamishra1a@gmail.com>
2025-09-02 20:45:26 +0530

    40607

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import RestaurantLocation
 
-admin.site.register(RestaurantLocation)
+@admin.register(RestaurantLocation)
+class RestaurantLocationAdmin(admin.ModelAdmin):
+    list_display = ()
+
 
