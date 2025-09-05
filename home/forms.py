51616fc467617430b10db98f2bbe878537bc8434
kanhamishra1 <kanhamishra1a@gmail.com>
2025-09-06 00:02:01 +0530

    42285

--- home/forms.py
+++ home/forms.py
@@ -5,4 +5,9 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email']
+
+class MailContact(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
          
