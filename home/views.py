0f61da00897cc55a909bc84607f9a0c0430bcd58
kanhamishra1 <kanhamishra1a@gmail.com>
2025-09-06 00:17:50 +0530

    42285

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponseServerError
-from .forms import ContactForm,Email
+from .forms import ContactForm,EmailContact
 from .models import RestaurantLocation
 from products.models import MenuItem
 
@@ -22,8 +22,12 @@ def demo_home_page(request):
     restaurant_name = "Testy Bites"
     return render(request, 'home/home1.html', {'restaurant_name' : restaurant_name})
 
+# For about section
+
 def about_view(request):
     return render(request, 'home/about.html')
+
+# For Reservation
 
 def reservation(request):
     try:
@@ -35,7 +39,10 @@ def reservation(request):
             "<h1>Something went wrong!</h1><p>Please try again later</p>"
         )
 
-def ContactForm(request):
+
+# for feedback
+
+def FeedbackForm(request):
     if request.method == "POST":
         form = Feedbackform(request.POST)
         if form.is_valid():
@@ -46,6 +53,9 @@ def ContactForm(request):
         form = ContactForm()
 
     return render(request, 'home/contact.html', {'form': form})
+
+
+# for search items
 
 def search(request):
     restaurant = Restaurant.objects.first()
@@ -62,3 +72,31 @@ def search(request):
         "query": query,
     }
     return render(request, "home.html", context)
+
+# For Email contact
+ def EmailContact(request):
+    if request.method == "POST":
+        form = EmailContact(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            # Send email
+            send_mail(
+                subject=f"New Contact Form Submission from {name}",
+                message=f"Name: {name}\nEmail: {email}\nMessage:\n{message}",
+                from_email=email
+                recipient_list=["TestyBites@gmail.com"],
+            )
+
+            return redirect("home")
+    else:
+        form = EmailContact()
+
+    return render(request, "home.html",{
+        "restaurant": restaurant,
+        "menu_items": menu_items,
+        "query": query,
+        "form": form,
+    })
