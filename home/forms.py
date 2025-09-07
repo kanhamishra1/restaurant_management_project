fd3b3642f72c259930c01cdf67e84d58700aad42
kanhamishra1 <kanhamishra1a@gmail.com>
2025-09-08 00:13:18 +0530

    43216

--- home/forms.py
+++ home/forms.py
@@ -1,13 +1,16 @@
 from django import forms
 from .models import Contact
 
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['name', 'email']
 
-class MailContact(forms.Form):
-    name = forms.CharField(max_length=100)
-    email = forms.EmailField()
-    message = forms.CharField(widget=forms.Textarea)
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100, required=True)
+    email = forms.EmailField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
+
+    def clean_message(self):
+        message = self.cleaned_data.get("message")
+        if len(massage) < 10:
+            raise forms.ValidationError("Message must be at least 10 character long.")
+        return message
          
