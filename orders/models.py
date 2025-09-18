741c4f03a9ce61a9afe754f27048caf143f2ad07
Balgopal Mishra <kanhamishra1a@gmail.com>
2025-09-18 22:47:57 +0530

    Order status

--- orders/models.py
+++ orders/models.py
@@ -13,15 +13,9 @@ class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     order_items = models.ManyToManyField(Menu)
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
-
-    
-    status = models.ForeignKey(
-        OrderStatus,
-        on_delete=models.SET_NULL,
-        null=True
-    )
-
+    status = models.ForeignKey(OrderStatus, on_delete=models.SET_NULL, null=True)
     ordered_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"Order #{self.pk} by {self.customer.username}"
